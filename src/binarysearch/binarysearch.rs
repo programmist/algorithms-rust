ecb3c22174cf04d0ce4704cfbf501b4fb398869c
Tony Childs <tony.childs@gmail.com>
2023-12-05 18:15:11 -0600

    binary search impl + benchmarks

--- src/binarysearch/binarysearch.rs
+++ src/binarysearch/binarysearch.rs
@@ -1,16 +1,16 @@
 pub fn search(nums: &[i32], key: i32) -> i32 {
-    let mut low = 0;
-    let mut high = nums.len() - 1;
+  let mut low = 0;
+  let mut high = nums.len() - 1;
 
-    while low <= high {
-        let mid: usize = low + (high - low) / 2;
-        if key < nums[mid] {
-            high = mid - 1;
-        } else if key > nums[mid] {
-            low = mid + 1;
-        } else {
-            return mid.try_into().unwrap();
-        }
+  while low <= high {
+    let mid: usize = low + (high - low) / 2;
+    if key < nums[mid] {
+      high = mid - 1;
+    } else if key > nums[mid] {
+      low = mid + 1;
+    } else {
+      return mid.try_into().unwrap();
     }
-    return -1;
+  }
+  return -1;
 }
--- src/binarysearch/main.rs
+++ src/binarysearch/main.rs
@@ -1,10 +1,66 @@
+use rand::Rng;
+use std::time::Instant;
+
 mod binarysearch;
 use binarysearch::search;
 
+fn get_random_array() -> [i32; 1000] {
+  let mut rng = rand::thread_rng();
+  let mut nums: [i32; 1000] = [0; 1000];
+  for i in 0..1000 {
+    nums[i] = rng.gen_range(0..100);
+  }
+  return nums;
+}
+
 fn main() {
-    let mut nums: [i32; 15] = [6, 33, 43, 51, 53, 13, 14, 93, 95, 96, 97, 25, 64, 72, 84];
-    nums.sort();
-    println!("{:?}", nums);
-    println!("search(&nums, 33)");
-    println!("{}", search(&nums, 33));
+  let mut nums: [i32; 1000] = get_random_array();
+  nums.sort();
+
+  let mut successes: Vec<core::time::Duration> = Vec::new();
+  let mut fails: Vec<core::time::Duration> = Vec::new();
+  let mut rng = rand::thread_rng();
+
+  for _ in 0..100 {
+    let key = rng.gen_range(0..1000);
+    println!("search for {}", key);
+    let start_time = Instant::now();
+    let result = search(&nums, key);
+    let elapsed_time = start_time.elapsed();
+
+    if result == -1 {
+      fails.push(elapsed_time);
+      println!("{} not found", key);
+    } else {
+      successes.push(elapsed_time);
+      println!("{} found at {}", key, result);
+    }
+  }
+
+  let success_sum: core::time::Duration = successes.iter().sum();
+  let success_size: i32 = successes.len().try_into().unwrap();
+  if success_size > 0 {
+    println!(
+      "Success avg time: {:?}",
+      success_sum / successes.len().try_into().unwrap()
+    );
+  } else {
+    println!("no successes")
+  }
+
+  let fail_sum: core::time::Duration = fails.iter().sum();
+  let fails_size: i32 = fails.len().try_into().unwrap();
+  if fails_size > 0 {
+    println!(
+      "Fail avg time: {:?}",
+      fail_sum / fails.len().try_into().unwrap()
+    );
+  } else {
+    println!("no fails")
+  }
+
+  println!(
+    "Avg total search times: {:?}",
+    (success_sum + fail_sum) / (successes.len() + fails.len()).try_into().unwrap()
+  );
 }
